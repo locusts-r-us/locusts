7b5626744d79586873994705e8bf6cd4f3f3e6cc
Matt Strapp <matt@mattstrapp.net>
2024-10-29 11:13:09 -0500

    Make JSR less upset and add a README
    Signed-off-by: Matt Strapp <matt@mattstrapp.net>

--- src/packages/locusts/mod.ts
+++ src/packages/locusts/mod.ts
@@ -1,1 +1,15 @@
+/**
+ * This module introduces locusts to your application.
+ *
+ * Why would you want to do that? I don't know, but you can.
+ *
+ * @example
+ * ```typescript
+ * import { introduceLocusts } from "@locusts-r-us/locusts"
+ *
+ * introduceLocusts()
+ * ```
+ * @module
+ */
+
 export * from "./src/index.ts"
